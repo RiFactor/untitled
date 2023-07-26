5fabaf6ddf60e40c3aa676af6ab63bc49727ac92
Ri <ri@3sidedcube.com>
2023-07-26 17:24:05 +0100

    Resolve console error for html table tags

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handleForLoop = () => {
+    for (let i = 0; i < 12; i++) {
+      console.log(i);
+    }
+  };
   const handleCalculation = (value: number) => {
     console.log(value);
   };
@@ -9,41 +16,68 @@ function App() {
   // state to remember sequence of events and numbers
   const numericValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  const [result, setResult] = useState("");
-
   return (
-    <div className="test">
+    <div>
       <div>
-        <p>Home</p>
-        <p>About</p>
-        <p> Contact Us</p>
+        <nav>
+          {/* links will be better */}
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/"> Contact Us</a>
+        </nav>
         <div>
           <h1>Calculator</h1>
+          <button onClick={() => handleForLoop()}>Click Me</button>
+
           <table>
-            <tr>Answer: {result}.</tr>
-            <tr>
-              <th></th>
-            </tr>
-            <tr>
-              <td>
-                {numericValues.map(number => {
-                  return (
-                    // find a way to get it to display a max of 3 per row
-                    // do a for loop to find out if it needs to go on a new row
-                    <button key={number} onClick={() => handleCalculation(number)}>
-                      {number}
-                    </button>
-                  );
-                })}
-              </td>
-              <button>C</button>
-              <button>AC</button>
-              <button>+</button>
-              <button>-</button>
-              <button>/</button>
-              <button>*</button>
-              <button>=</button>
-            </tr>
+            <thead>
+              <tr>
+                <td>Answer: {result}.</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th></th>
+              </tr>
+              <tr>
+                <td>
+                  {numericValues.map(number => {
+                    return (
+                      // find a way to get it to display a max of 3 per row
+                      // do a for loop to find out if it needs to go on a new row
+                      // for (i = 0; i < 12; i ++) {
+                      <button key={number} onClick={() => handleCalculation(number)}>
+                        {number}
+                      </button>
+                      // }
+                    );
+                  })}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button>C</button>
+                </td>
+                <td>
+                  <button>AC</button>
+                </td>
+                <td>
+                  <button>+</button>
+                </td>
+                <td>
+                  <button>-</button>
+                </td>
+                <td>
+                  <button>/</button>
+                </td>
+                <td>
+                  <button>*</button>
+                </td>
+                <td>
+                  <button>=</button>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
