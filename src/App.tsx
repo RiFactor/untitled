51598019baadd7448fbe6c24d388a403a1ef77c5
Ri <ri@3sidedcube.com>
2023-07-26 11:37:54 +0100

    What: Why:

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="test">
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
