b61b37f2ee73555ad98e0e453be79a59d645ff79
Ri <ri@3sidedcube.com>
2023-07-26 14:25:49 +0100

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ function App() {
   return (
     <>
       <div className="test2">
-
-
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
