5f3fc1fdf7b2f5c33ddde2ba4957aca8975c7246
Ri <ri@3sidedcube.com>
2023-07-31 11:03:46 +0100

    Build fix; prev Push was resolving lock files?

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ interface Calculator {
 // state to remember sequence of events and numbers
 const numericValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const reverseNumericValues = numericValues.reverse();
-const bonusOperators = ["C", "AC", "+/-"];
+// const bonusOperators = ["C", "AC", "+/-"];
 const operators = ["/", "*", "-", "+", "="];
 
 function App() {
