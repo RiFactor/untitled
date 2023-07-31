cdb78bc09a98ad04408e4df7ce16560fe1c53d6e
Ri <ri@3sidedcube.com>
2023-07-31 16:41:02 +0100

    Tidy

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,4 +1,4 @@
-import { HStack, Switch, useColorMode, Text, VStack } from "@chakra-ui/react";
+import { HStack, Switch, useColorMode, Text } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
