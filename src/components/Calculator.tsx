1176039efda28e1cbffeb51e9903b3af893cd5d0
Ri <ri@3sidedcube.com>
2023-08-03 12:14:44 +0100

    Key events work a bit

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -11,14 +11,51 @@ const Calculator = () => {
   const numericValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // leave these as they are, not enum
   const reverseNumericValues = numericValues.reverse();
   useEffect(() => {
-    window.addEventListener("keypress", event => {
-      console.log(event.key, "pressed");
-      if (parseInt(event.key)) {
-        dispatch({ type: "number", payload: parseInt(event.key) }); // want it to happen once
+    console.log("load");
+    window.addEventListener("keydown", event => {
+      console.log(event.key);
+      // switch (event.key) {
+      //   case event.key:
+      //     dispatch({ type: "number", payload: parseInt(event.key) }); // want it to happen once
+      // }
+      // switch case here how??
+      console.log(parseInt(event.key));
+      if (parseInt(event.key) || event.key === "0") {
+        // this isn't working
+        console.log("number");
+        dispatch({ type: "number", payload: parseInt(event.key) });
+      } else if (
+        (event.shiftKey && event.key === "*") ||
+        (event.shiftKey && event.key === "+") ||
+        event.key === "/" ||
+        event.key === "=" ||
+        event.key === "-"
+      ) {
+        // this isn't working
+        console.log(event.key, "test operator");
+        dispatch({ type: "operator", payload: event.key as EOperators }); // want it to happen once
+        // make enter key "="
+      } else if (event.key === "Enter") {
+        console.log("entere heEre?");
+        dispatch({ type: "operator", payload: "=" as EOperators });
       }
       // add signal to abort (cleanup when leaving page)
-      if (event.key === "c" || "C") {
+      else if (event.key === "c" || event.key === "C") {
+        console.log(event.key, "clear last value");
         dispatch({ type: "clear_last_value" });
+      } else if (event.key === "a" || event.key === "A") {
+        console.log(event.key, "clear all");
+        dispatch({ type: "clear_all" });
+      } else if (event.key === "`") {
+        console.log(event.key, "sign inversion");
+        dispatch({ type: "sign_inversion" });
+      } else if (parseInt(event.key) || event.key === "0") {
+        // this isn't working
+        console.log("number");
+        dispatch({ type: "number", payload: parseInt(event.key) }); // want it to happen once
+      } else {
+        console.log(event.key, "else");
+        return; // do nothing, not clear!! --> is it b/c page is being reloaded?
       }
     });
   }, []);
@@ -50,7 +87,7 @@ const Calculator = () => {
   return (
     <main className="gap-4">
       <h1 className="">Calculator</h1>
-      <div className="m-4 flex max-w-[250px] flex-col gap-2 rounded border-2 p-4 dark:border-neutral-400">
+      <div className="m-4 flex max-w-[260px] flex-col gap-2 rounded border-2 p-4 dark:border-neutral-400">
         <h2 className="mb-3 rounded border-2 border-neutral-400 p-3.5 text-right text-4xl font-bold leading-8 tracking-tight">
           {display()}
         </h2>
