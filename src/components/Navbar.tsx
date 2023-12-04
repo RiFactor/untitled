c59eb0e7db66585beee403d243bf18557b1e36a5
Ri <ri@3sidedcube.com>
2023-12-04 15:26:57 +0000

    Translation attempt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
 import ColorModeSwitch from "components/ColorModeSwitch";
 import NavButton from "./ui/NavButton";
+import { FormattedMessage } from "react-intl";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between gap-2 p-4">
       <div className="flex gap-5">
-        <NavButton name="Home" to="/"></NavButton>
+        <NavButton name="Home" to="/">
+          {/* <FormattedMessage id="example" /> */}
+        </NavButton>
         <NavButton name="Calculator" to="calculator"></NavButton>
         <NavButton name="Drawing Pad" to="drawing-pad"></NavButton>
       </div>
--- src/components/ui/NavButton.tsx
+++ src/components/ui/NavButton.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import { ReactNode } from "react";
 import { NavLink } from "react-router-dom";
 
 interface IProps {
   name?: string;
   to?: any;
+  children?: ReactNode;
 }
 
-const NavButton = ({ name, to }: IProps) => {
+const NavButton = ({ name, to, children }: IProps) => {
   return (
     <NavLink
       to={to}
       className={({ isActive }) => `nav-link font-bold w-18 ${isActive && "dark:text-cyan-300 underline "} `}
     >
       {name}
+      {children}
     </NavLink>
   );
 };
