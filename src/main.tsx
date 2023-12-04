25c0830451664a47c9c5097507122aead0e8efc1
Ri <ri@3sidedcube.com>
2023-12-04 15:19:06 +0000

    React-intl not working

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,18 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes.tsx";
 import theme from "./theme.ts";
+import { IntlProvider } from "react-intl";
+import translations from "./locales/en.json";
+
+const mergedMessages = Object.assign({}, translations);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {/* <IntlProvider messages={mergedMessages} locale="en" defaultLocale="en"> */}
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
+    {/* </IntlProvider> */}
   </React.StrictMode>
 );
